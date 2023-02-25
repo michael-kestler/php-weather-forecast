094f7046f64f82d86257139e2d31c8692e044296
Michael Kestler <michael.s.kestler@gmail.com>
2023-02-24 22:58:23 -0800

    did basic styling of input

--- index.php
+++ index.php
@@ -21,8 +21,19 @@
     <title>Weather Scraper</title>
 </head>
 <body>
+<div class="container text-center px-4 py-5 my-5">
+        <h1>What's The Weather?</h1>
+        <p>Enter the name of a city.</p>
+        <form method="post">
+            <fieldset class="form-group">
+                <!-- <label for="city">Enter the name of a city.</label> -->
+                <input type="text" id="city" name="city" class="w-25">
+            </fieldset>
+            <button type="submit" id="submit" class="btn btn-primary btn-sm px-4 mt-2">Submit</button>
+        </form>
+</div>
 
-
+    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
